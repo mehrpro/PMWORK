c5d5a398848386c2da91f4384fde93b254912f16
farshid mohammadi <fm708801@gmail.com>
2021-06-23 14:48:50 +0430

    455454

--- PMWORK/CodingForms/SubGroupForm.cs
+++ PMWORK/CodingForms/SubGroupForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PMWORK.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,11 +11,32 @@ using System.Windows.Forms;
 
 namespace PMWORK.CodingForms
 {
-    public partial class SubGroupForm : Form
+    public partial class SubGroupForm : DevExpress.XtraEditors.XtraForm
     {
+        private AppDbContext db;
         public SubGroupForm()
         {
             InitializeComponent();
+            db = new AppDbContext();
+        }
+
+        private void UpdateList(int companyid,int group)
+        {
+            var qry = db.SubGroups.Where(x=>x.CompanyID_FK == companyid && x.GroupID_FK == group).ToList();
+            var list = new List<SubGroup>();
+            if (qry.Count > 0)
+            {
+                foreach (var item in qry)
+                {
+                    list.Add(new SubGroup()
+                    {
+                        ID = item.ID,
+                        SubGroupIndex = item.SubGroupIndex,
+                        SubGroupTitle = item.SubGroupTitle,
+                        Description = item.Description
+                    });
+                }
+            }
         }
     }
 }
---
+++ PMWORK/CodingForms/ViewModels/SubGroupViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PMWORK.CodingForms.ViewModels
+{
+    public class SubGroupViewModel
+    {
+        [Display(AutoGenerateField = false)]
+        public int ID { get; set; }
+        [Display(Name ="کد")]
+        public byte SubGroupIndex { get; set; }
+        [Display(Name ="عنوان")]
+        public string SubGroupTitle { get; set; }
+        [Display(Name ="توضیحات")]
+        public string Description { get; set; }
+    }
+}
